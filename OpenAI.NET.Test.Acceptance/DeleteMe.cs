8cd0d82decdc030ab8d565a8fd5f9ecdddee354f
Brian Parker <brian.parker@hotmail.com.au>
2023-03-11 16:27:32 +1100

    INFRA: Initialize Acceptance Project

---
+++ OpenAI.NET.Test.Acceptance/DeleteMe.cs
@@ -0,0 +1,14 @@
+// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using Xunit;
+
+namespace OpenAI.NET.Test.Acceptance
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(condition: true);
+    }
+}
