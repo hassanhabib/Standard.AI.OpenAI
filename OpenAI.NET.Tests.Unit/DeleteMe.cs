b7e9fbb90a00a3ff7b4b462a5338452957103821
Brian Parker <brian.parker@hotmail.com.au>
2023-03-11 15:39:59 +1100

    INFRA: Initialize Unit Test Project

---
+++ OpenAI.NET.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,14 @@
+// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using Xunit;
+
+namespace OpenAI.NET.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(condition: true);
+    }
+}
