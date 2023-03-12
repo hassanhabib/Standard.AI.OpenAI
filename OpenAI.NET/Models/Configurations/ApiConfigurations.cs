d88efb7aed68904b2de343c5f5cd7ae43369b595
Hassan Habib <hassanhabib@live.com>
2023-03-12 13:05:03 -0700

    BROKERS: Initialize API Broker

---
+++ OpenAI.NET/Models/Configurations/ApiConfigurations.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+namespace OpenAI.NET.Models.Configurations
+{
+    internal class ApiConfigurations
+    {
+        public string ApiUrl { get; set; }
+        public string ApiKey { get; set; }
+        public string OrganizationId { get; set; }
+    }
+}
