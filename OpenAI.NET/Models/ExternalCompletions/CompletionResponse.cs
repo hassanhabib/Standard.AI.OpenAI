55403780642dc298ea6b21f74a425656ea28f241
Hassan Habib <hassanhabib@live.com>
2023-03-13 22:58:59 -0700

    DATA: CompletionResponse Magic!

--- OpenAI.NET/Models/ExternalCompletions/CompletionResponse.cs
+++ OpenAI.NET/Models/ExternalCompletions/CompletionResponse.cs
@@ -15,25 +15,37 @@ namespace OpenAI.NET.Models.ExternalCompletions
     {
         [JsonProperty("id")]
         public string id { get; set; }
+        [JsonProperty("_object")]
         public string _object { get; set; }
+        [JsonProperty("created")]
         public int created { get; set; }
+        [JsonProperty("model")]
         public string model { get; set; }
-        public Choice[] choices { get; set; }
+        publ[JsonProperty("choices")]ic Choice
+            [] choices { get; set; }
+        [JsonProperty("usage")]
         public Usage usage { get; set; }
     }
 
     public class Usage
     {
+        [JsonProperty("prompt_tokens")]
         public int prompt_tokens { get; set; }
+        [JsonProperty("completion_tokens")]
         public int completion_tokens { get; set; }
+        [JsonProperty("total_tokens")]
         public int total_tokens { get; set; }
     }
 
     public class Choice
     {
+        [JsonProperty("text")]
         public string text { get; set; }
+        [JsonProperty("index")]
         public int index { get; set; }
+        [JsonProperty("logprobs")]
         public object logprobs { get; set; }
+        [JsonProperty("finish_reason")]
         public string finish_reason { get; set; }
     }
 
