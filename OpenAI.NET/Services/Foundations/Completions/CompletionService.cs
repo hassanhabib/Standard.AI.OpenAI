1f7e7ddc2b99616531c6081cc632d4811187a482
Brian Parker <brian.parker@hotmail.com.au>
2023-03-15 18:26:25 +1100

    ShouldPromptCompletionAsync -> WIP

--- OpenAI.NET/Services/Foundations/Completions/CompletionService.cs
+++ OpenAI.NET/Services/Foundations/Completions/CompletionService.cs
@@ -3,9 +3,11 @@
 // ---------------------------------------------------------------
 
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using OpenAI.NET.Brokers.OpenAIs;
 using OpenAI.NET.Models.Completions;
+using OpenAI.NET.Models.ExternalCompletions;
 
 namespace OpenAI.NET.Services.Foundations.Completions
 {
@@ -16,9 +18,65 @@ namespace OpenAI.NET.Services.Foundations.Completions
         public CompletionService(IOpenAIBroker openAiBroker) =>
             this.openAiBroker = openAiBroker;
 
-        public ValueTask<Completion> PromptCompletionAsync(Completion completion)
+        public async ValueTask<Completion> PromptCompletionAsync(Completion completion)
         {
-            throw new NotImplementedException();
+            ExternalCompletionRequest externalCompletionRequest = ConvertToCompletionRequest(completion);
+            ExternalCompletionResponse completionResponse = await this.openAiBroker.PostCompletionRequestAsync(externalCompletionRequest);
+
+            return completion;
+        }
+
+        private ExternalCompletionRequest ConvertToCompletionRequest(Completion completion)
+        {
+            return new ExternalCompletionRequest
+            {
+                Model = completion.Request.Model,
+                BestOf = completion.Request.BestOf,
+                CompletionsPerPrompt = completion.Request.CompletionsPerPrompt,
+                Echo = completion.Request.Echo,
+                FrequencyPenalty = completion.Request.FrequencyPenalty,
+                LogitBias = completion.Request.LogitBias,
+                LogProbabilities = completion.Request.LogProbabilities,
+                MaxTokens = completion.Request.MaxTokens,
+                PresencePenalty = completion.Request.PresencePenalty,
+                ProbabilityMass = completion.Request.ProbabilityMass,
+                Prompt = completion.Request.Prompt,
+                Stop = completion.Request.Stop,
+                Stream = completion.Request.Stream,
+                Suffix = completion.Request.Suffix,
+                Temperature = completion.Request.Temperature,
+                User = completion.Request.User
+            };
+        }
+
+        private Completion ConvertToCompletion(Completion completion, ExternalCompletionResponse externalCompletionResponse)
+        {
+            completion.Response = new CompletionResponse
+            {
+                Model = externalCompletionResponse.Model,
+
+                Choices = externalCompletionResponse.Choices.Select(externalChoice => new Choice
+                {
+                    FinishReason = externalChoice.FinishReason,
+                    Index = externalChoice.Index,
+                    LogProbabilities = externalChoice.LogProbabilities,
+                    Text = externalChoice.Text
+                }).ToArray(),
+
+                Created = externalCompletionResponse.Created,
+                Id = externalCompletionResponse.Id,
+
+                Usage = new Usage
+                {
+                    CompletionTokens = externalCompletionResponse.Usage.CompletionTokens,
+                    PromptTokens = externalCompletionResponse.Usage.PromptTokens,
+                    TotalTokens = externalCompletionResponse.Usage.TotalTokens
+                },
+
+                Object = externalCompletionResponse.Object
+            };
+
+            return completion;
         }
     }
 }
