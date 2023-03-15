753574dfacea07fe8f54a1efd944505354bff2a8
Hassan Habib <hassanhabib@live.com>
2023-03-14 23:09:51 -0700

    ShouldPromptCompletionAsync -> WIP

--- OpenAI.NET.Tests.Unit/Foundations/Completions/CompletionServiceTests.cs
+++ OpenAI.NET.Tests.Unit/Foundations/Completions/CompletionServiceTests.cs
@@ -2,6 +2,8 @@
 // Copyright (c) Coalition of the Good-Hearted Engineers 
 // ---------------------------------------------------------------
 
+using System.Collections.Generic;
+using System.IO;
 using Moq;
 using OpenAI.NET.Brokers.OpenAIs;
 using OpenAI.NET.Models.Completions;
@@ -22,6 +24,44 @@ namespace OpenAI.NET.Tests.Unit.Foundations.Completions
             this.completionService = new CompletionService(
                 openAiBroker: this.openAiBrokerMock.Object);
         }
+
+        private static dynamic CreateRandomCompletionProperties()
+        {
+            return new
+            {
+                Model = CreateRandomString(),
+                Prompt = CreateRandomStringArray(),
+                Suffix = CreateRandomString(),
+                MaxTokens = GetRandomNumber(),
+                Temperature = GetRandomNumber(),
+                ProbabilityMass = GetRandomNumber(),
+                CompletionsPerPrompt = GetRandomNumber(),
+                Stream = GetRandomBoolean(),
+                LogProbabilities = GetRandomNumber(),
+                Echo = GetRandomBoolean(),
+                Stop = CreateRandomStringArray(),
+                PresencePenalty = GetRandomNumber(),
+                FrequencyPenalty = GetRandomNumber(),
+                BestOf = GetRandomNumber(),
+                LogitBias = CreateRandomDictionary(),
+                User = CreateRandomString()
+            };
+        }
+
+        private static string CreateRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 0, max: 10).GetValue();
+
+        private static string[] CreateRandomStringArray() =>
+            new Filler<string[]>().Create();
+
+        private static bool GetRandomBoolean() =>
+            new SequenceGeneratorBoolean().GetValue();
+
+        private static Dictionary<string, int> CreateRandomDictionary() =>
+            new Filler<Dictionary<string, int>>().Create();
 
         private static Completion CreateRandomCompletion() =>
             CreateCompletionFiller().Create();
