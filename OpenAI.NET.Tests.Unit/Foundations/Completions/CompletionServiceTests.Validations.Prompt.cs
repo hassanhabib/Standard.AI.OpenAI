2ce403e5657eeb3b679e856b11e7ad10bd2fc18d
Hassan Habib <hassanhabib@live.com>
2023-03-15 22:48:16 -0700

    ShouldThrowValidationExceptionOnPromptIfCompletionIsInvalidAsync -> FAIL

--- OpenAI.NET.Tests.Unit/Foundations/Completions/CompletionServiceTests.Validations.Prompt.cs
+++ OpenAI.NET.Tests.Unit/Foundations/Completions/CompletionServiceTests.Validations.Prompt.cs
@@ -2,10 +2,8 @@
 // Copyright (c) Coalition of the Good-Hearted Engineers 
 // ---------------------------------------------------------------
 
-using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
-using Force.DeepCloner;
 using Moq;
 using OpenAI.NET.Models.Completions;
 using OpenAI.NET.Models.Completions.Exceptions;
@@ -23,20 +21,58 @@ namespace OpenAI.NET.Tests.Unit.Foundations.Completions
             Completion nullCompletion = null;
             var nullCompletionException = new NullCompletionException();
 
-            var exceptedCompletionValidationException = 
+            var exceptedCompletionValidationException =
                 new CompletionValidationException(nullCompletionException);
 
             // when
-            ValueTask<Completion> promptCompletionTask = 
+            ValueTask<Completion> promptCompletionTask =
                 this.completionService.PromptCompletionAsync(nullCompletion);
-            
-            CompletionValidationException actualCompletionValidationException = 
+
+            CompletionValidationException actualCompletionValidationException =
                 await Assert.ThrowsAsync<CompletionValidationException>(
                     promptCompletionTask.AsTask);
 
             // then
             actualCompletionValidationException.Should()
                 .BeEquivalentTo(exceptedCompletionValidationException);
+
+            this.openAiBrokerMock.Verify(broker =>
+                broker.PostCompletionRequestAsync(
+                    It.IsAny<ExternalCompletionRequest>()),
+                        Times.Never);
+
+            this.openAiBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnPromptIfCompletionIsInvalidAsync()
+        {
+            // given
+            var invalidCompletion = new Completion();
+            invalidCompletion.Request = null;
+
+            var invalidCompletionException =
+                new InvalidCompletionException();
+
+            invalidCompletionException.AddData(
+                key: nameof(Completion.Request),
+                values: "Object is required");
+
+            var expectedCompletionValidationException =
+                new CompletionValidationException(
+                    invalidCompletionException);
+
+            // when
+            ValueTask<Completion> promptCompletionTask =
+                this.completionService.PromptCompletionAsync(invalidCompletion);
+
+            CompletionValidationException actualCompletionValidationException =
+                await Assert.ThrowsAsync<CompletionValidationException>(
+                    promptCompletionTask.AsTask);
+
+            // then
+            actualCompletionValidationException.Should()
+                .BeEquivalentTo(expectedCompletionValidationException);
 
             this.openAiBrokerMock.Verify(broker =>
                 broker.PostCompletionRequestAsync(
---
+++ OpenAI.NET/Models/Completions/Exceptions/InvalidCompletionException.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace OpenAI.NET.Models.Completions.Exceptions
+{
+    internal class InvalidCompletionException : Xeption
+    {
+        public InvalidCompletionException()
+            : base(message: "Invalid completion error occurred, fix errors and try again.")
+        { }
+    }
+}
