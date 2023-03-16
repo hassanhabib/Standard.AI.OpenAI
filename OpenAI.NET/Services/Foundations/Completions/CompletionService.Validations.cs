9fe5e85ef4717495d1a2377e7dd3422656285897
Kailu Hu <hukailu@piorsoft.com>
2023-03-15 23:30:03 -0700

    ShouldThrowValidationExceptionOnPromptIfCompletionIsInvalidAsync -> PASS

--- OpenAI.NET/Services/Foundations/Completions/CompletionService.Validations.cs
+++ OpenAI.NET/Services/Foundations/Completions/CompletionService.Validations.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Coalition of the Good-Hearted Engineers 
 // ---------------------------------------------------------------
 
+using System;
 using OpenAI.NET.Models.Completions;
 using OpenAI.NET.Models.Completions.Exceptions;
 
@@ -12,9 +13,12 @@ namespace OpenAI.NET.Services.Foundations.Completions
         private void ValidateCompletion(Completion completion)
         {
             ValidateCompletionNotNull(completion);
+            ValidateCompletionRequestNotNull(completion.Request);
 
             Validate(
-                (Rule: IsInvalid(completion.Request), Parameter: nameof(Completion.Request)));
+                (Rule: IsInvalid(completion.Request), Parameter: nameof(Completion.Request)),
+                (Rule: IsInvalid(completion.Request.Model), Parameter: nameof(Completion.Request.Model)),
+                (Rule: IsInvalid(completion.Request.Prompt), Parameter: nameof(Completion.Request.Prompt)));
         }
 
         private void ValidateCompletionNotNull(Completion completion)
@@ -25,10 +29,28 @@ namespace OpenAI.NET.Services.Foundations.Completions
             }
         }
 
+        private void ValidateCompletionRequestNotNull(CompletionRequest request)
+        {
+            Validate(
+                (Rule: IsInvalid(request), Parameter: nameof(Completion.Request)));
+        }
+
         private dynamic IsInvalid(object @object) => new
         {
             Condition = @object is null,
             Message = "Object is required"
+        };
+
+        private dynamic IsInvalid(string text) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(text),
+            Message = "Value is required"
+        };
+
+        private dynamic IsInvalid(string[] textArray) => new
+        {
+            Condition = textArray is null,
+            Message = "Value is required"
         };
 
         private void Validate(params (dynamic Rule, string Parameter)[] validations)
