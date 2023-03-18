9203b13145ba3f70e37c0370a34d7e98afb83cf2
Hassan Habib <hassanhabib@live.com>
2023-03-17 22:26:41 -0700

    CLIENTS: Completion Client
    Closes #37

---
+++ OpenAI.NET/Models/Clients/Completions/Exceptions/CompletionClientValidationException.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace OpenAI.NET.Models.Clients.Completions.Exceptions
+{
+    internal class CompletionClientValidationException : Xeption
+    {
+        public CompletionClientValidationException(Xeption innerException)
+            : base(message: "Completion client validation error occurred, fix errors and try again.",
+                   innerException)
+        { }
+    }
+}
