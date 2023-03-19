ec5e7c69fbcb992c8abc2e25b83a0e0155905202
Hassan Habib <hassanhabib@live.com>
2023-03-18 23:32:03 -0700

    ShouldPromptCompletionAsync -> WIP

---
+++ OpenAI.NET.Tests.Acceptance/Clients/Completions/CompletionClientTests.Prompts.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Newtonsoft.Json;
+using OpenAI.NET.Models.Completions;
+using OpenAI.NET.Models.ExternalCompletions;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using Xunit;
+
+namespace OpenAI.NET.Tests.Acceptance.Clients.Completions
+{
+    public partial class CompletionClientTests
+    {
+        [Fact]
+        public async Task ShouldPromptCompletionAsync()
+        {
+            // given
+            Completion randomCompletion = CreateRandomCompletion();
+            Completion inputCompletion = randomCompletion;
+
+            ExternalCompletionRequest completionRequest = 
+                ConvertToCompletionRequest(inputCompletion);
+
+            ExternalCompletionResponse completionResponse =
+                CreateRandomExternalCompletionResponse();
+
+            Completion expectedCompletion = inputCompletion.DeepClone();
+            expectedCompletion = ConvertToCompletion(inputCompletion, completionResponse);
+
+            var jsonSerializationSettings = new JsonSerializerSettings(); 
+            jsonSerializationSettings.DefaultValueHandling = DefaultValueHandling.Ignore;
+
+            this.wireMockServer.Given(
+                Request.Create()
+                .WithPath("/v1/completions")
+                .WithHeader("Authorization", $"Bearer {this.apiKey}")
+                .WithHeader("OpenAI-Organization", $"{this.organizationId}")
+                .WithBody(JsonConvert.SerializeObject(
+                    completionRequest,
+                    jsonSerializationSettings)))
+                .RespondWith(
+                    Response.Create()
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithBodyAsJson(completionResponse));
+
+            // when
+            Completion actualCompletion =
+                await this.openAIClient.Completions.PromptCompletionAsync(
+                    inputCompletion);
+
+            // then
+            actualCompletion.Should().BeEquivalentTo(expectedCompletion);
+        }
+    }
+}
