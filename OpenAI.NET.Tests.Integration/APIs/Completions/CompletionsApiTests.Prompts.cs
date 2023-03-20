a67ced35a68c60c9e7b99d4266a67f00bdee98db
Hassan Habib <hassanhabib@live.com>
2023-03-19 23:20:17 -0700

    ShouldPromptCompletionAsync -> PASS

---
+++ OpenAI.NET.Tests.Integration/APIs/Completions/CompletionsApiTests.Prompts.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using OpenAI.NET.Models.Services.Foundations.Completions;
+using Xunit;
+
+namespace OpenAI.NET.Tests.Integration.APIs.Completions
+{
+    public partial class CompletionsApiTests
+    {
+        [Fact(Skip = "This test is only for releases")]
+        public async Task ShouldPromptCompletionAsync()
+        {
+            // given
+            var inputCompletion = new Completion
+            {
+                Request = new CompletionRequest
+                {
+                    Model = "text-davinci-003",
+                    Prompt = new string[]
+                    {
+                        "Human: What's the tallest building in the world?\n",
+                        "AI: "
+                    }
+                }
+            };
+
+            // when
+            Completion responseCompletion =
+                await this.openAIClient.Completions.PromptCompletionAsync(
+                    inputCompletion);
+
+            // then
+            Assert.NotNull(responseCompletion.Response);
+        }
+    }
+}
