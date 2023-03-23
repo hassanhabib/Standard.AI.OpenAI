4bdcc16f954aee9e28430c7545d421a5e37d202d
Hassan Habib <hassanhabib@live.com>
2023-03-23 00:20:18 -0700

    CODE RUB: Fix Chat Completion Request Parameter Name

--- Standard.AI.OpenAI/Brokers/OpenAIs/OpenAIBroker.ChatCompletions.cs
+++ Standard.AI.OpenAI/Brokers/OpenAIs/OpenAIBroker.ChatCompletions.cs
@@ -9,12 +9,12 @@ namespace Standard.AI.OpenAI.Brokers.OpenAIs
 {
     internal partial class OpenAIBroker
     {
-        public async ValueTask<ExternalChatCompletionResponse> PostChatCompletionAsync(
-            ExternalChatCompletionRequest chatCompletionRequest)
+        public async ValueTask<ExternalChatCompletionResponse> PostChatCompletionRequestAsync(
+            ExternalChatCompletionRequest externalChatCompletionRequest)
         {
             return await PostAsync<ExternalChatCompletionRequest, ExternalChatCompletionResponse>(
                 relativeUrl: "v1/chat/completions",
-                content: chatCompletionRequest);
+                content: externalChatCompletionRequest);
         }
     }
 }
