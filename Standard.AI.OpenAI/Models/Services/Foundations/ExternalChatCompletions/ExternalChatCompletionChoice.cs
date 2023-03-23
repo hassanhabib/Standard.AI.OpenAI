a3c7ee95a46a0c775a0cd4ac499d60bbdc327bb1
Brian Parker <brian.parker@hotmail.com.au>
2023-03-23 16:23:28 +1100

    DATA: External Chat Models

---
+++ Standard.AI.OpenAI/Models/Services/Foundations/ExternalChatCompletions/ExternalChatCompletionChoice.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using Newtonsoft.Json;
+
+namespace Standard.AI.OpenAI.Models.Services.Foundations.ExternalChatCompletions
+{
+    internal class ExternalChatCompletionChoice
+    {
+        [JsonProperty(propertyName: "index")]
+        public int Index { get; set; }
+
+        [JsonProperty(propertyName: "message")]
+        public ExternalChatCompletionMessage Message { get; set; }
+
+        [JsonProperty(propertyName: "finish_reason")]
+        public string FinishReason { get; set; }
+    }
+}
---
+++ Standard.AI.OpenAI/Models/Services/Foundations/ExternalChatCompletions/ExternalChatCompletionRequest.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Standard.AI.OpenAI.Models.Services.Foundations.ExternalChatCompletions
+{
+    internal class ExternalChatCompletionRequest
+    {
+        [JsonProperty(propertyName: "model")]
+        public string Model { get; set; }
+
+        [JsonProperty(propertyName: "messages")]
+        public ExternalChatCompletionMessage[] Messages { get; set; } =
+            Array.Empty<ExternalChatCompletionMessage>();
+
+        [JsonProperty(propertyName: "temperature")]
+        public double Temperature { get; set; } = 1;
+
+        [JsonProperty(propertyName: "top_p")]
+        public double ProbabilityMass { get; set; } = 1;
+
+        [JsonProperty(propertyName: "n")]
+        public int CompletionsPerPrompt { get; set; } = 1;
+
+        [JsonProperty(propertyName: "stream")]
+        public bool Stream { get; set; } = false;
+
+        [JsonProperty(propertyName: "stop")]
+        public string[] Stop { get; set; } = null;
+
+        [JsonProperty(propertyName: "max_tokens")]
+        public int MaxTokens { get; set; } = 16;
+
+        [JsonProperty(propertyName: "presence_penalty")]
+        public double PresencePenalty { get; set; } = 0;
+
+        [JsonProperty(propertyName: "frequency_penalty")]
+        public double FrequencyPenalty { get; set; } = 0;
+
+        [JsonProperty(propertyName: "logit_bias")]
+        public Dictionary<string, int> LogitBias { get; set; } =
+            new Dictionary<string, int>();
+
+        [JsonProperty(propertyName: "user")]
+        public string User { get; set; } = string.Empty;
+    }
+}
