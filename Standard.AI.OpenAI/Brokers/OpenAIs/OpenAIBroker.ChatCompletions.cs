5b44fd754acebb7c6a113673989b2bc512a383dd
ElbekDeveloper <e.normurodov@mail.ru>
2023-03-23 10:33:19 +0500

    BROKERS: Post Chat Completion

---
+++ Standard.AI.OpenAI/Brokers/OpenAIs/OpenAIBroker.ChatCompletions.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Standard.AI.OpenAI.Models.Services.Foundations.ExternalChatCompletions;
+
+namespace Standard.AI.OpenAI.Brokers.OpenAIs
+{
+    internal partial class OpenAIBroker
+    {
+        public async ValueTask<ExternalChatCompletionResponse> PostChatCompletionAsync(
+            ExternalChatCompletionRequest chatCompletionRequest)
+        {
+            return await PostAsync<ExternalChatCompletionRequest, ExternalChatCompletionResponse>(
+                relativeUrl: "v1/chat/completions",
+                content: chatCompletionRequest);
+        }
+    }
+}
