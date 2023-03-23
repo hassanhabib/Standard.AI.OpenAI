cca0f98d85f08a11cb7e67a77f62598cb9263df9
Brian Parker <brian.parker@hotmail.com.au>
2023-03-23 22:21:32 +1100

    DATA: External Models

---
+++ Standard.AI.OpenAI/Models/Services/Foundations/ExternalModels/ExternalModel.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using Newtonsoft.Json;
+
+namespace Standard.AI.OpenAI.Models.Services.Foundations.ExternalModels
+{
+    internal class ExternalModel
+    {
+        [JsonProperty(propertyName: "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(propertyName: "_object")]
+        public string _object { get; set; }
+
+        [JsonProperty(propertyName: "created")]
+        public int Created { get; set; }
+
+        [JsonProperty(propertyName: "owned_by")]
+        public string Owned_by { get; set; }
+
+        [JsonProperty(propertyName: "permission")]
+        public ExternalPermission[] Permission { get; set; }
+
+        [JsonProperty(propertyName: "root")]
+        public string Root { get; set; }
+
+        [JsonProperty(propertyName: "parent")]
+        public object Parent { get; set; }
+    }
+}
---
+++ Standard.AI.OpenAI/Models/Services/Foundations/ExternalModels/ExternalPermission.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using Newtonsoft.Json;
+
+namespace Standard.AI.OpenAI.Models.Services.Foundations.ExternalModels
+{
+    internal class ExternalPermission
+    {
+        [JsonProperty(propertyName: "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(propertyName: "_object")]
+        public string Object { get; set; }
+
+        [JsonProperty(propertyName: "created")]
+        public int Created { get; set; }
+
+        [JsonProperty(propertyName: "allow_create_engine")]
+        public bool AllowCreateEngine { get; set; }
+
+        [JsonProperty(propertyName: "allow_sampling")]
+        public bool AllowSampling { get; set; }
+
+        [JsonProperty(propertyName: "allow_logprobs")]
+        public bool AllowLogprobs { get; set; }
+
+        [JsonProperty(propertyName: "allow_search_indices")]
+        public bool AllowSearchIndices { get; set; }
+
+        [JsonProperty(propertyName: "allow_view")]
+        public bool AllowView { get; set; }
+
+        [JsonProperty(propertyName: "allow_fine_tuning")]
+        public bool AllowFineTuning { get; set; }
+
+        [JsonProperty(propertyName: "organization")]
+        public string Organization { get; set; }
+
+        [JsonProperty(propertyName: "group")]
+        public object Group { get; set; }
+
+        [JsonProperty(propertyName: "is_blocking")]
+        public bool IsBlocking { get; set; }
+    }
+}
