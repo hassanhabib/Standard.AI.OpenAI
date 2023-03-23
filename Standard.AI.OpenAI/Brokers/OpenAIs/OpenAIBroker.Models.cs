25533a936a6e27e16be4b4a8cacf3500cb6ce436
Brian Parker <brian.parker@hotmail.com.au>
2023-03-23 22:38:29 +1100

    BROKERS: Get all External Models

---
+++ Standard.AI.OpenAI/Brokers/OpenAIs/OpenAIBroker.Models.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Standard.AI.OpenAI.Models.Services.Foundations.ExternalModels;
+
+namespace Standard.AI.OpenAI.Brokers.OpenAIs
+{
+    internal partial class OpenAIBroker
+    {
+        public async ValueTask<ExternalModel[]> GetAllModelsAsync() =>
+            await GetAsync<ExternalModel[]>(relativeUrl: "v1/models");
+    }
+}
