340170cf0ef440e830de35bda6ebc4e8201cccd5
Ricardo Cruz <ricardo@mediatradecraft.com>
2023-03-24 02:45:38 -0400

    ShouldSendChatCompletionAsync -> PASS

--- Standard.AI.OpenAI/Services/Foundations/ChatCompletions/ChatCompletionService.cs
+++ Standard.AI.OpenAI/Services/Foundations/ChatCompletions/ChatCompletionService.cs
@@ -3,9 +3,13 @@
 // ---------------------------------------------------------------
 
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Standard.AI.OpenAI.Brokers.OpenAIs;
 using Standard.AI.OpenAI.Models.Services.Foundations.ChatCompletions;
+using Standard.AI.OpenAI.Models.Services.Foundations.Completions;
+using Standard.AI.OpenAI.Models.Services.Foundations.ExternalChatCompletions;
+using Standard.AI.OpenAI.Models.Services.Foundations.ExternalCompletions;
 
 namespace Standard.AI.OpenAI.Services.Foundations.ChatCompletions
 {
@@ -16,9 +20,79 @@ namespace Standard.AI.OpenAI.Services.Foundations.ChatCompletions
         public ChatCompletionService(IOpenAIBroker openAIBroker) =>
             this.openAIBroker = openAIBroker;
 
-        public ValueTask<ChatCompletion> SendChatCompletionAsync(ChatCompletion chatCompletion)
+        public async ValueTask<ChatCompletion> SendChatCompletionAsync(ChatCompletion chatCompletion)
         {
-            throw new NotImplementedException();
+            ExternalChatCompletionRequest externalChatCompletionRequest = 
+                ConvertToChatCompletionRequest(chatCompletion);
+
+            ExternalChatCompletionResponse externalChatCompletionResponse =
+                await this.openAIBroker.PostChatCompletionRequestAsync(externalChatCompletionRequest);
+
+            return ConvertToChatCompletion(chatCompletion,externalChatCompletionResponse);
+        }
+
+        private static ExternalChatCompletionRequest ConvertToChatCompletionRequest(ChatCompletion chatCompletion)
+        {
+            return new ExternalChatCompletionRequest
+            {
+                Model = chatCompletion.Request.Model,
+
+                Messages = chatCompletion.Request.Messages.Select(message =>
+                {
+                    return new ExternalChatCompletionMessage
+                    {
+                        Role = message.Role,
+                        Content = message.Content
+                    };
+                }).ToArray(),
+
+                Temperature = chatCompletion.Request.Temperature,
+                ProbabilityMass = chatCompletion.Request.ProbabilityMass,
+                CompletionsPerPrompt = chatCompletion.Request.CompletionsPerPrompt,
+                Stream = chatCompletion.Request.Stream,
+                Stop = chatCompletion.Request.Stop,
+                MaxTokens = chatCompletion.Request.MaxTokens,
+                PresencePenalty = chatCompletion.Request.PresencePenalty,
+                FrequencyPenalty = chatCompletion.Request.FrequencyPenalty,
+                LogitBias = chatCompletion.Request.LogitBias,
+                User = chatCompletion.Request.User
+            };
+        }
+
+        private static ChatCompletion ConvertToChatCompletion(
+            ChatCompletion chatCompletion,
+            ExternalChatCompletionResponse externalChatCompletionResponse)
+        {
+
+            chatCompletion.Response = new ChatCompletionResponse
+            {
+               Id = externalChatCompletionResponse.Id,
+               Created = externalChatCompletionResponse.Created,
+               Choices = externalChatCompletionResponse.Choices.Select(choice =>
+               {
+                   return new ChatCompletionChoice
+                   {
+                       FinishReason = choice.FinishReason,
+                       Index = choice.Index,
+
+                       Message = new ChatCompletionMessage
+                       {
+                           Content = choice.Message.Content,
+                           Role = choice.Message.Role
+                       }
+                   };
+
+               }).ToArray(),
+               Usage = new ChatCompletionUsage
+               {
+                   CompletionTokens = externalChatCompletionResponse.Usage.CompletionTokens,
+                   PromptTokens = externalChatCompletionResponse.Usage.PromptTokens,
+                   TotalTokens = externalChatCompletionResponse.Usage.TotalTokens
+               },
+               Object = externalChatCompletionResponse.Object
+            };
+
+            return chatCompletion;
         }
     }
 }
