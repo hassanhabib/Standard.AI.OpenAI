ddfa287822b48be68b728b89627d9147d9447103
Brian Parker <brian.parker@hotmail.com.au>
2023-03-25 17:06:15 +1100

    ShouldThrowValidationExceptionOnSendIfRequestIsInvalidAsync -> PASS

--- Standard.AI.OpenAI/Services/Foundations/ChatCompletions/ChatCompletionService.Validations.cs
+++ Standard.AI.OpenAI/Services/Foundations/ChatCompletions/ChatCompletionService.Validations.cs
@@ -2,9 +2,11 @@
 // Copyright (c) Coalition of the Good-Hearted Engineers 
 // ---------------------------------------------------------------
 
+using System;
+using System.Data;
+using System.Reflection.Metadata;
 using Standard.AI.OpenAI.Models.Services.Foundations.ChatCompletions;
 using Standard.AI.OpenAI.Models.Services.Foundations.ChatCompletions.Exceptions;
-using System.Data;
 
 namespace Standard.AI.OpenAI.Services.Foundations.ChatCompletions
 {
@@ -17,6 +19,13 @@ namespace Standard.AI.OpenAI.Services.Foundations.ChatCompletions
             Validate(
                 (Rule: IsInvalid(chatCompletion.Request),
                 Parameter: nameof(ChatCompletion.Request)));
+
+            Validate(
+                (Rule: IsInvalid(chatCompletion.Request.Messages),
+                Parameter: nameof(ChatCompletionRequest.Messages)),
+
+                (Rule: IsInvalid(chatCompletion.Request.Model),
+                Parameter: nameof(ChatCompletionRequest.Model)));
         }
 
         private void ValidateChatCompletionIsNotNull(ChatCompletion chatCompletion)
@@ -27,9 +36,15 @@ namespace Standard.AI.OpenAI.Services.Foundations.ChatCompletions
             }
         }
 
-        private static dynamic IsInvalid(object @object) => new 
+        private static dynamic IsInvalid(object @object) => new
         {
             Condition = @object is null,
+            Message = "Value is required"
+        };
+
+        private static dynamic IsInvalid(string text) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(text),
             Message = "Value is required"
         };
 
