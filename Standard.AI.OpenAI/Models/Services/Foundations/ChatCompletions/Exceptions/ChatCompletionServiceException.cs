a0ae4d107c966914646b1c23c9d4aff4c1c2bb57
Ricardo Cruz <ricardo@mediatradecraft.com>
2023-03-26 02:50:32 -0400

    CODE RUB: Changed to public for exception class

--- Standard.AI.OpenAI/Models/Services/Foundations/ChatCompletions/Exceptions/ChatCompletionServiceException.cs
+++ Standard.AI.OpenAI/Models/Services/Foundations/ChatCompletions/Exceptions/ChatCompletionServiceException.cs
@@ -7,7 +7,7 @@ using Xeptions;
 
 namespace Standard.AI.OpenAI.Models.Services.Foundations.ChatCompletions.Exceptions
 {
-    internal class ChatCompletionServiceException : Xeption
+    public class ChatCompletionServiceException : Xeption
     {
         public ChatCompletionServiceException(Exception innerException)
             : base(message: "Chat completion service error occurred, contact support.", innerException)
