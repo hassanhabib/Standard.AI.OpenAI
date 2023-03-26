0546f3de54d79ead52695dfccbc0bdfda4202fc0
Ricardo Cruz <ricardo@mediatradecraft.com>
2023-03-26 02:51:22 -0400

    CODE RUB: Changed to public for ExcessiveCallChatCompletionException

--- Standard.AI.OpenAI/Models/Services/Foundations/ChatCompletions/Exceptions/ExcessiveCallChatCompletionException.cs
+++ Standard.AI.OpenAI/Models/Services/Foundations/ChatCompletions/Exceptions/ExcessiveCallChatCompletionException.cs
@@ -7,7 +7,7 @@ using Xeptions;
 
 namespace Standard.AI.OpenAI.Models.Services.Foundations.ChatCompletions.Exceptions
 {
-    internal class ExcessiveCallChatCompletionException : Xeption
+    public class ExcessiveCallChatCompletionException : Xeption
     {
         public ExcessiveCallChatCompletionException(Exception innerException)
             : base(message: "Excessive call error occurred, limit your calls.", innerException)
