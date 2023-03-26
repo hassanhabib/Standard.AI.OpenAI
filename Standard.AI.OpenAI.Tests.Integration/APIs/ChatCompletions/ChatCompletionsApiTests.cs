c007912be80a83451b36b0056e883df1d8f98306
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2023-03-26 10:11:03 +0200

    ShouldSendChatCompletionAsync -> PASS

---
+++ Standard.AI.OpenAI.Tests.Integration/APIs/ChatCompletions/ChatCompletionsApiTests.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using Standard.AI.OpenAI.Clients.OpenAIs;
+using Standard.AI.OpenAI.Models.Configurations;
+
+namespace Standard.AI.OpenAI.Tests.Integration.APIs.ChatCompletions
+{
+    public partial class ChatCompletionsApiTests
+    {
+        private readonly IOpenAIClient openAIClient;
+
+        public ChatCompletionsApiTests()
+        {
+            var openAIConfigurations = new ApiConfigurations
+            {
+                ApiKey = Environment.GetEnvironmentVariable("ApiKey"),
+                OrganizationId = Environment.GetEnvironmentVariable("OrgId"),
+                ApiUrl = "https://api.openai.com/"
+            };
+
+            this.openAIClient = new OpenAIClient(openAIConfigurations);
+        }
+    }
+}
