c6369977b955698dc9e4b513eddc1ee90d3bcf2b
Gummimundur <gudm.mar@hotmail.com>
2023-03-27 23:02:32 +0000

    DATA: External Image Generation Model

---
+++ Standard.AI.OpenAI/Models/Services/Foundations/ExternalImageGenerations/ExternalImageGenerationRequest.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using Newtonsoft.Json;
+
+namespace Standard.AI.OpenAI.Models.Services.Foundations.ExternalImageGenerations
+{
+    internal class ExternalImageGenerationRequest
+    {
+        [JsonProperty(propertyName: "prompt")]
+        public string Prompt { get; set; }
+
+        [JsonProperty(propertyName: "n")]
+        public int ImagesToGenerate { get; set; } = 1;
+
+        [JsonProperty(propertyName: "size")]
+        public string ImageSize { get; set; } = "1024x1024";
+
+        [JsonProperty(propertyName: "response_format")]
+        public string ResponseFormat { get; set; } = "url";
+
+        [JsonProperty(propertyName: "user")]
+        public string User { get; set; } = String.Empty;
+    }
+}
