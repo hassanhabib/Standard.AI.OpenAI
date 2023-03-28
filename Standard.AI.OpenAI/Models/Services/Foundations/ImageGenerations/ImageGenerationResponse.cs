65732186cefffa952bfde138b1dadced999d5bfd
Gummimundur <gudm.mar@hotmail.com>
2023-03-28 20:45:08 +0000

    DATA: Image Generation Model

---
+++ Standard.AI.OpenAI/Models/Services/Foundations/ImageGenerations/ImageGenerationResponse.cs
@@ -0,0 +1,12 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+namespace Standard.AI.OpenAI.Models.Services.Foundations.ImageGenerations
+{
+    public class ImageGenerationResponse
+    {
+        public int Created { get; set; }
+        public ImageGenerationResult[] Results { get; set; }
+    }
+}
