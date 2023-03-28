42e7789578c522f1d8343a0dfd44c171a7024145
Diogo Duarte <diogoduarte@stratioautomotive.com>
2023-03-28 23:59:39 +0100

    DATA: Create `struct` for Model Name

--- Standard.AI.OpenAI/Models/Services/Foundations/Completions/ModelName.cs
+++ Standard.AI.OpenAI/Models/Services/Foundations/Completions/ModelName.cs
@@ -6,19 +6,19 @@ namespace Standard.AI.OpenAI.Models.Services.Foundations.Completions
 {
     public readonly struct ModelName
     {
-        public static string Ada => "ada";
-        public static string AdaV1 => "text-ada-001";
-        public static string Babbage => "babbage";
-        public static string BabbageV1 => "text-babbage-001";
-        public static string Curie => "curie";
-        public static string CurieInstructBeta => "curie-instruct-beta";
-        public static string CurieV1 => "text-curie-001";
-        public static string Davinci => "davinci";
-        public static string DavinciInstructBeta => "davinci-instruct-beta";
-        public static string DavinciV1 => "text-davinci-001";
-        public static string DavinciV2 => "text-davinci-002";
-        public static string DavinciV3 => "text-davinci-003";
-        public static string CodexDavinciV2 => "code-davinci-002";
-        public static string CodexCushmanV1 => "code-cushman-001";
+        public const string Ada = "ada";
+        public const string AdaV1 = "text-ada-001";
+        public const string Babbage = "babbage";
+        public const string BabbageV1 = "text-babbage-001";
+        public const string CodexCushmanV1 = "code-cushman-001";
+        public const string CodexDavinciV2 = "code-davinci-002";
+        public const string Curie = "curie";
+        public const string CurieInstructBeta = "curie-instruct-beta";
+        public const string CurieV1 = "text-curie-001";
+        public const string Davinci = "davinci";
+        public const string DavinciInstructBeta = "davinci-instruct-beta";
+        public const string DavinciV1 = "text-davinci-001";
+        public const string DavinciV2 = "text-davinci-002";
+        public const string DavinciV3 = "text-davinci-003";
     }
 }
