234fc42a58b170d3249ceec1a4872d909c66cdd7
Gummimundur <gudm.mar@hotmail.com>
2023-03-28 21:36:08 +0000

    ShouldGenerateImageAsync -> PASS

--- Standard.AI.OpenAI/Services/Foundations/ImageGenerations/ImageGenerationService.cs
+++ Standard.AI.OpenAI/Services/Foundations/ImageGenerations/ImageGenerationService.cs
@@ -2,8 +2,10 @@
 // Copyright (c) Coalition of the Good-Hearted Engineers 
 // ---------------------------------------------------------------
 
+using System.Linq;
 using System.Threading.Tasks;
 using Standard.AI.OpenAI.Brokers.OpenAIs;
+using Standard.AI.OpenAI.Models.Services.Foundations.ExternalImageGenerations;
 using Standard.AI.OpenAI.Models.Services.Foundations.ImageGenerations;
 
 namespace Standard.AI.OpenAI.Services.Foundations.ImageGenerations
@@ -15,9 +17,57 @@ namespace Standard.AI.OpenAI.Services.Foundations.ImageGenerations
         public ImageGenerationService(IOpenAIBroker openAIBroker) =>
             this.openAIBroker = openAIBroker;
 
-        public ValueTask<ImageGeneration> GenerateImageAsync(ImageGeneration imageGeneration)
+        public async ValueTask<ImageGeneration> GenerateImageAsync(ImageGeneration imageGeneration)
         {
-            throw new System.NotImplementedException();
+            ExternalImageGenerationResponse externalImageGenerationResponse =
+                await PostImageGenerationRequestAsync(imageGeneration);
+
+            return ConvertToImageGeneration(imageGeneration, externalImageGenerationResponse);
+        }
+
+        private async ValueTask<ExternalImageGenerationResponse> PostImageGenerationRequestAsync(
+            ImageGeneration imageGeneration)
+        {
+            ExternalImageGenerationRequest externalImageGenerationRequest =
+                ConvertToImageGenerationRequest(imageGeneration);
+
+            ExternalImageGenerationResponse externalImageGenerationResponse =
+                await this.openAIBroker.PostImageGenerationRequestAsync(externalImageGenerationRequest);
+
+            return externalImageGenerationResponse;
+        }
+
+        private static ExternalImageGenerationRequest ConvertToImageGenerationRequest(ImageGeneration imageGeneration)
+        {
+            return new ExternalImageGenerationRequest
+            {
+                Prompt = imageGeneration.Request.Prompt,
+                ImagesToGenerate = imageGeneration.Request.ImagesToGenerate,
+                ImageSize = imageGeneration.Request.ImageSize,
+                ResponseFormat = imageGeneration.Request.ResponseFormat,
+                User = imageGeneration.Request.User
+            };
+        }
+
+        private static ImageGeneration ConvertToImageGeneration(
+            ImageGeneration imageGeneration,
+            ExternalImageGenerationResponse externalImageGenerationResponse)
+        {
+            imageGeneration.Response = new ImageGenerationResponse
+            {
+                Created = externalImageGenerationResponse.Created,
+
+                Results = externalImageGenerationResponse.Results.Select(result =>
+                {
+                    return new ImageGenerationResult
+                    {
+                        ImageUrl = result.ImageUrl,
+                        Base64EncodedJsonImage = result.Base64EncodedJsonImage
+                    };
+                }).ToArray()
+            };
+
+            return imageGeneration;
         }
     }
 }
