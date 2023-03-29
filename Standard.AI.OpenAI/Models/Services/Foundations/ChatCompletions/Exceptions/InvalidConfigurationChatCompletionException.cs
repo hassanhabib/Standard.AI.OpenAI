aa3698a12d54fd68979fdf774b8010e535ee6916
Gummimundur <gudm.mar@hotmail.com>
2023-03-29 19:23:32 +0000

    CODE RUB: Change All Internal Exceptions To Public

--- Standard.AI.OpenAI/Models/Services/Foundations/ChatCompletions/Exceptions/InvalidConfigurationChatCompletionException.cs
+++ Standard.AI.OpenAI/Models/Services/Foundations/ChatCompletions/Exceptions/InvalidConfigurationChatCompletionException.cs
@@ -7,7 +7,7 @@ using Xeptions;
 
 namespace Standard.AI.OpenAI.Models.Services.Foundations.ChatCompletions.Exceptions
 {
-    internal class InvalidConfigurationChatCompletionException : Xeption
+    public class InvalidConfigurationChatCompletionException : Xeption
     {
         public InvalidConfigurationChatCompletionException(Exception innerException)
             : base(message: "Invalid chat completion configuration error occurred, contact support.", innerException)
