1a12f5887a2b00f7f7da91780c73c85162a5971c
Ricardo Cruz <ricardo@mediatradecraft.com>
2023-03-29 02:16:54 -0400

    BROKERS: Create DateTime

---
+++ Standard.AI.OpenAI/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using System;
+
+namespace Standard.AI.OpenAI.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset ConvertToDateTimeOffSet(uint totalSeconds) =>
+            DateTimeOffset.FromUnixTimeSeconds(totalSeconds);
+    }
+}
---
+++ Standard.AI.OpenAI/Brokers/DateTimes/IDateTimeBroker.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using System;
+
+namespace Standard.AI.OpenAI.Brokers.DateTimes
+{
+    public interface IDateTimeBroker
+    {
+        DateTimeOffset ConvertToDateTimeOffSet(uint totalSeconds);
+    }
+}
