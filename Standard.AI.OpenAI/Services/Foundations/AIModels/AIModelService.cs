c7600b48b57c340bb68d5dac92e7c23027fccb6a
Kailu Hu <hukailu@piorsoft.com>
2023-03-29 23:20:44 -0700

    ShouldRetrieveAllAIModelsAsync -> WIP

--- Standard.AI.OpenAI/Services/Foundations/AIModels/AIModelService.cs
+++ Standard.AI.OpenAI/Services/Foundations/AIModels/AIModelService.cs
@@ -3,10 +3,12 @@
 // ---------------------------------------------------------------
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Standard.AI.OpenAI.Brokers.DateTimes;
 using Standard.AI.OpenAI.Brokers.OpenAIs;
 using Standard.AI.OpenAI.Models.Services.Foundations.AIModels;
+using Standard.AI.OpenAI.Models.Services.Foundations.ExternalAIModels;
 
 namespace Standard.AI.OpenAI.Services.Foundations.AIModels
 {
@@ -15,13 +17,48 @@ namespace Standard.AI.OpenAI.Services.Foundations.AIModels
         private readonly IOpenAIBroker openAIBroker;
         private readonly IDateTimeBroker dateTimeBroker;
 
-        public AIModelService(IOpenAIBroker openAIBroker, IDateTimeBroker dateTimeBroker)
+        public AIModelService(
+            IOpenAIBroker openAIBroker, 
+            IDateTimeBroker dateTimeBroker)
         {
             this.openAIBroker = openAIBroker;
             this.dateTimeBroker = dateTimeBroker;
         }
 
-        public ValueTask<IEnumerable<AIModel>> RetrieveAllAIModelsAsync() =>
-            throw new System.NotImplementedException();
+        public async ValueTask<IEnumerable<AIModel>> RetrieveAllAIModelsAsync()
+        {
+            ExternalAIModelsResult externalAIModelsResult = 
+                await this.openAIBroker.GetAllAIModelsAsync();
+
+            return externalAIModelsResult.AIModels.Select(externalAIModel =>
+                new AIModel
+                {
+                    Name = externalAIModel.Id,
+                    CreatedDate = this.dateTimeBroker.ConvertToDateTimeOffSet(externalAIModel.Created),
+                    Type = externalAIModel.Object,
+                    OwnedBy = externalAIModel.OwnedBy,
+                    Parent = externalAIModel.Parent,
+                    OriginModel = externalAIModel.Root,
+
+                    Permissions = externalAIModel.Permissions.Select(
+                        externalPermission =>
+                        {
+                            return new AIModelPermission
+                            {
+                                Id = externalPermission.Id,
+                                Type = externalPermission.Object,
+                                CreatedDate = this.dateTimeBroker.ConvertToDateTimeOffSet(externalPermission.Created),
+                                AllowCreateEngine = externalPermission.AllowCreateEngine,
+                                AllowSampling = externalPermission.AllowSampling,
+                                AllowLogProbabilities = externalPermission.AllowLogprobs,
+                                AllowSearchIndices = externalPermission.AllowSearchIndices,
+                                AllowView = externalPermission.AllowView,
+                                AllowFineTuning = externalPermission.AllowFineTuning,
+                                Organization = externalPermission.Organization,
+                                IsBlocking = externalPermission.IsBlocking
+                            };
+                        }).ToArray()
+                }).ToArray();
+        }
     }
 }
