fa4d562d8c0d70e3ed218e82c455363ac08defd5
Gummimundur <gudm.mar@hotmail.com>
2023-03-30 09:58:20 +0000

    ShouldGenerateImageAsync -> PASS

---
+++ Standard.AI.OpenAI.Tests.Integration/APIs/ImageGenerations/ImageGenerationApiTests.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using Standard.AI.OpenAI.Clients.OpenAIs;
+using Standard.AI.OpenAI.Models.Configurations;
+
+namespace Standard.AI.OpenAI.Tests.Integration.APIs.ImageGenerations
+{
+    public partial class ImageGenerationApiTests
+    {
+        private readonly IOpenAIClient openAIClient;
+
+        public ImageGenerationApiTests()
+        {
+            var openAIConfigurations = new ApiConfigurations
+            {
+                ApiKey = Environment.GetEnvironmentVariable("ApiKey"),
+                OrganizationId = Environment.GetEnvironmentVariable("OrgId"),
+                ApiUrl = "https://api.openai.com/"
+            };
+
+            this.openAIClient = new OpenAIClient(openAIConfigurations);
+        }
+    }
+}
