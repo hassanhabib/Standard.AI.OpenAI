fdbcebad36bcd5056af97afdba64c999067e5bae
Gummimundur <gudm.mar@hotmail.com>
2023-03-30 08:32:38 +0000

    MINOR FIX: Change ImageGenerationResponse Created to DateTimeOffset

--- Standard.AI.OpenAI/Models/Services/Foundations/ImageGenerations/ImageGenerationResponse.cs
+++ Standard.AI.OpenAI/Models/Services/Foundations/ImageGenerations/ImageGenerationResponse.cs
@@ -2,11 +2,13 @@
 // Copyright (c) Coalition of the Good-Hearted Engineers 
 // ---------------------------------------------------------------
 
+using System;
+
 namespace Standard.AI.OpenAI.Models.Services.Foundations.ImageGenerations
 {
     public class ImageGenerationResponse
     {
-        public int Created { get; set; }
+        public DateTimeOffset Created { get; set; }
         public ImageGenerationResult[] Results { get; set; }
     }
 }
