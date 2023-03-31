365a4c16d87b3f40cd62959193346856ca51ddc3
Catalin Andronie <catalin.andronie@outlook.com>
2023-03-31 12:42:29 +0300

    DATA: External Audio Transcription Model

---
+++ Standard.AI.OpenAI/Models/Services/Foundations/ExternalAudioTranscriptions/ExternalAudioTranscriptionRequest.cs
@@ -0,0 +1,30 @@
+// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.IO;
+using RESTFulSense.Models.Attributes;
+
+namespace Standard.AI.OpenAI.Models.Services.Foundations.ExternalAudioTranscriptions
+{
+    internal class ExternalAudioTranscriptionRequest
+    {
+        [RESTFulStreamContent(name: "file")]
+        public Stream File { get; set; }
+
+        [RESTFulFileName(name: "file")]
+        public string FileName { get; set; }
+
+        [RESTFulStringContent(name: "model")]
+        public string Model { get; set; }
+
+        [RESTFulStringContent(name: "prompt")]
+        public string Prompt { get; set; }
+
+        [RESTFulStringContent(name: "temperature")]
+        public decimal Temperature { get; set; }
+
+        [RESTFulStringContent(name: "language")]
+        public string Language { get; set; }
+    }
+}
