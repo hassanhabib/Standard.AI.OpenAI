14bc7321d651f2a9f4b7c31cc3abaadd00f9fedf
Brian Parker <brian.parker@hotmail.com.au>
2023-04-03 00:17:18 +1000

    CODE RUB: Remove extra space in parameters

--- Standard.AI.OpenAI/Brokers/OpenAIs/OpenAIBroker.cs
+++ Standard.AI.OpenAI/Brokers/OpenAIs/OpenAIBroker.cs
@@ -41,7 +41,7 @@ namespace Standard.AI.OpenAI.Brokers.OpenAIs
                 ignoreDefaultValues: true);
         }
 
-        private async ValueTask<Stream> PostWithStreamResponseAsync<TRequest>(string relativeUrl, TRequest content, CancellationToken cancellationToken )
+        private async ValueTask<Stream> PostWithStreamResponseAsync<TRequest>(string relativeUrl, TRequest content, CancellationToken cancellationToken)
         {
             return await this.apiClient.PostContentWithStreamResponseAsync(
                 relativeUrl,
