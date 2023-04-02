a4f7d40fcaf4612b65ca06a90e7fbce8548a08ad
Catalin-Andronie <catalin.andronie@gmail.com>
2023-04-02 10:37:44 +0300

    ShouldThrowDependencyExceptionOnSendIfUrlNotFoundAsync -> FAIL

---
+++ Standard.AI.OpenAI.Tests.Unit/Services/Foundations/AudioTranscriptions/AudioTranscriptionServiceTests.Exceptions.Send.cs
@@ -0,0 +1,62 @@
+// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using RESTFulSense.Exceptions;
+using Standard.AI.OpenAI.Models.Services.Foundations.AudioTranscriptions;
+using Standard.AI.OpenAI.Models.Services.Foundations.AudioTranscriptions.Exceptions;
+using Standard.AI.OpenAI.Models.Services.Foundations.ExternalAudioTranscriptions;
+using Xunit;
+
+namespace Standard.AI.OpenAI.Tests.Unit.Services.Foundations.AudioTranscriptions
+{
+    public partial class AudioTranscriptionServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnSendIfUrlNotFoundAsync()
+        {
+            // given
+            AudioTranscription someAudioTranscription =
+                CreateRandomAudioTranscription();
+
+            var httpResponseUrlNotFoundException =
+                new HttpResponseUrlNotFoundException();
+
+            var invalidConfigurationAudioTranscriptionException =
+                new InvalidConfigurationAudioTranscriptionException(
+                    httpResponseUrlNotFoundException);
+
+            var expectedAudioTranscriptionDependencyException =
+                new AudioTranscriptionDependencyException(
+                    invalidConfigurationAudioTranscriptionException);
+
+            this.openAIBrokerMock.Setup(broker =>
+                broker.PostAudioTranscriptionRequestAsync(
+                    It.IsAny<ExternalAudioTranscriptionRequest>()))
+                        .ThrowsAsync(httpResponseUrlNotFoundException);
+
+            // when
+            ValueTask<AudioTranscription> sendAudioTranscriptionTask =
+                this.audioTranscriptionService.SendAudioTranscriptionAsync(someAudioTranscription);
+
+            AudioTranscriptionDependencyException
+                actualAudioTranscriptionDependencyException =
+                    await Assert.ThrowsAsync<AudioTranscriptionDependencyException>(
+                        sendAudioTranscriptionTask.AsTask);
+
+            // then
+            actualAudioTranscriptionDependencyException.Should().BeEquivalentTo(
+                expectedAudioTranscriptionDependencyException);
+
+            this.openAIBrokerMock.Verify(broker =>
+                broker.PostAudioTranscriptionRequestAsync(
+                    It.IsAny<ExternalAudioTranscriptionRequest>()),
+                        Times.Once);
+
+            this.openAIBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
