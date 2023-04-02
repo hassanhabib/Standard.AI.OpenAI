f9fbbb2ca07c650ba596bd439340d774b0442476
Gummimundur <gudm.mar@hotmail.com>
2023-04-02 13:06:16 +0000

    DATA: Remove Created

--- Standard.AI.OpenAI/Models/Services/Foundations/Completions/CompletionResponse.cs
+++ Standard.AI.OpenAI/Models/Services/Foundations/Completions/CompletionResponse.cs
@@ -10,7 +10,6 @@ namespace Standard.AI.OpenAI.Models.Services.Foundations.Completions
     {
         public string Id { get; set; }
         public string Object { get; set; }
-        public int Created { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
         public string Model { get; set; }
         public Choice[] Choices { get; set; }
