0f602a325d64fe690c4cb6b2695de97e4f6eff3b
Brian Parker <brian.parker@hotmail.com.au>
2023-04-03 01:25:54 +1000

    CODE RUB: PostCompletionRequestWithStreamResponseAsync missing in interface

--- Standard.AI.OpenAI/Brokers/OpenAIs/IOpenAIBroker.Completions.cs
+++ Standard.AI.OpenAI/Brokers/OpenAIs/IOpenAIBroker.Completions.cs
@@ -2,6 +2,8 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Standard.AI.OpenAI.Models.Services.Foundations.ExternalCompletions;
 
@@ -9,6 +11,12 @@ namespace Standard.AI.OpenAI.Brokers.OpenAIs
 {
     internal partial interface IOpenAIBroker
     {
-        ValueTask<ExternalCompletionResponse> PostCompletionRequestAsync(ExternalCompletionRequest completionRequest);
+        ValueTask<ExternalCompletionResponse> PostCompletionRequestAsync(
+            ExternalCompletionRequest
+            completionRequest);
+
+        ValueTask<Stream> PostCompletionRequestWithStreamResponseAsync(
+            ExternalCompletionRequest completionRequest,
+            CancellationToken cancellationToken);
     }
 }
