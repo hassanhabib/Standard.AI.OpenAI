c557edaa97b582b16a929aff69f105a761191ef0
Brian Parker <brian.parker@hotmail.com.au>
2023-04-03 00:44:10 +1000

    BROKERS: Chat Completions Stream Response

--- Standard.AI.OpenAI/Brokers/OpenAIs/OpenAIBroker.ChatCompletions.cs
+++ Standard.AI.OpenAI/Brokers/OpenAIs/OpenAIBroker.ChatCompletions.cs
@@ -2,6 +2,8 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Standard.AI.OpenAI.Models.Services.Foundations.ExternalChatCompletions;
 
@@ -15,6 +17,16 @@ namespace Standard.AI.OpenAI.Brokers.OpenAIs
             return await PostAsync<ExternalChatCompletionRequest, ExternalChatCompletionResponse>(
                 relativeUrl: "v1/chat/completions",
                 content: externalChatCompletionRequest);
+        }
+
+        public async ValueTask<Stream> PostChatCompletionRequestWithStreamResponseAsync(
+            ExternalChatCompletionRequest externalChatCompletionRequest,
+            CancellationToken cancellationToken)
+        {
+            return await PostWithStreamResponseAsync(
+                relativeUrl: "v1/chat/completions",
+                externalChatCompletionRequest,
+                cancellationToken);
         }
     }
 }
