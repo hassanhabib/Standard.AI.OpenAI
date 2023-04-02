0c73213d4f57fdfdad3ad30d998ddc445817e151
Gummimundur <gudm.mar@hotmail.com>
2023-04-02 13:00:56 +0000

    DATA: Add CreatedDate

--- Standard.AI.OpenAI/Models/Services/Foundations/Completions/CompletionResponse.cs
+++ Standard.AI.OpenAI/Models/Services/Foundations/Completions/CompletionResponse.cs
@@ -2,6 +2,8 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System;
+
 namespace Standard.AI.OpenAI.Models.Services.Foundations.Completions
 {
     public class CompletionResponse
@@ -9,6 +11,7 @@ namespace Standard.AI.OpenAI.Models.Services.Foundations.Completions
         public string Id { get; set; }
         public string Object { get; set; }
         public int Created { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
         public string Model { get; set; }
         public Choice[] Choices { get; set; }
         public Usage Usage { get; set; }
