79e13526ddc9ba601ce314df11c8eccdcf049286
ElbekDeveloper <e.normurodov@mail.ru>
2023-04-04 11:34:46 +0500

    DOCUMENTATIONS: Add XML Explanations

--- Standard.AI.OpenAI/Models/Clients/Completions/Exceptions/CompletionClientDependencyException.cs
+++ Standard.AI.OpenAI/Models/Clients/Completions/Exceptions/CompletionClientDependencyException.cs
@@ -6,6 +6,10 @@ using Xeptions;
 
 namespace Standard.AI.OpenAI.Models.Clients.Completions.Exceptions
 {
+    /// <summary>
+    /// This exception is thrown when a dependency error occurs while using the completion client. 
+    /// For example, if a required dependency is unavailable or incompatible.
+    /// </summary>
     public class CompletionClientDependencyException : Xeption
     {
         public CompletionClientDependencyException(Xeption innerException)
