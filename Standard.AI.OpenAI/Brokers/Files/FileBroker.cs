32e40f1156dd7c4d730ac44b33c9880a680a68f4
ElbekDeveloper <e.normurodov@mail.ru>
2023-04-05 11:19:32 +0500

    BROKERS: Read File

---
+++ Standard.AI.OpenAI/Brokers/Files/FileBroker.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using System.IO;
+
+namespace Standard.AI.OpenAI.Brokers.Files
+{
+    internal class FileBroker : IFileBroker
+    {
+        public Stream ReadFile(string path) =>
+            File.OpenRead(path);
+    }
+}
---
+++ Standard.AI.OpenAI/Brokers/Files/IFileBroker.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+using System.IO;
+
+namespace Standard.AI.OpenAI.Brokers.Files
+{
+    internal interface IFileBroker
+    {
+        Stream ReadFile(string path);
+    }
+}
