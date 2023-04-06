3fd5658f96316b8693e76850b64555725b842fc3
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-04-06 23:20:50 +0500

    DOCUMENTATIONS: Document ChatCompletionClient Methods & Exceptions

--- Standard.AI.OpenAI/Clients/ChatCompletions/IChatCompletionsClient.cs
+++ Standard.AI.OpenAI/Clients/ChatCompletions/IChatCompletionsClient.cs
@@ -3,12 +3,16 @@
 // ----------------------------------------------------------------------------------
 
 using System.Threading.Tasks;
+using Standard.AI.OpenAI.Models.Clients.ChatCompletions.Exceptions;
 using Standard.AI.OpenAI.Models.Services.Foundations.ChatCompletions;
 
 namespace Standard.AI.OpenAI.Clients.ChatCompletions
 {
     public interface IChatCompletionsClient
     {
+        /// <exception cref="ChatCompletionClientValidationException" />
+        /// <exception cref="ChatCompletionClientDependencyException" />
+        /// <exception cref="ChatCompletionClientServiceException" />
         ValueTask<ChatCompletion> SendChatCompletionAsync(ChatCompletion chatCompletion);
     }
 }
--- Standard.AI.OpenAI/Models/Clients/ChatCompletions/Exceptions/ChatCompletionClientValidationException.cs
+++ Standard.AI.OpenAI/Models/Clients/ChatCompletions/Exceptions/ChatCompletionClientValidationException.cs
@@ -8,6 +8,10 @@ namespace Standard.AI.OpenAI.Models.Clients.ChatCompletions.Exceptions
 {
     public class ChatCompletionClientValidationException : Xeption
     {
+        /// <summary>
+        /// This exception is thrown when a validation error occurs while using the Chat completion client.
+        /// For example, if required data is missing or invalid.
+        /// </summary>
         public ChatCompletionClientValidationException(Xeption innerException)
             : base(message: "Chat completion client validation error occurred, fix errors and try again.",
                    innerException)
