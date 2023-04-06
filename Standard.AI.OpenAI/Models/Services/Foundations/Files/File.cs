af07a24d1903b4d9d18e052b151e14c0e16f9f79
Gummimundur <gudm.mar@hotmail.com>
2023-04-06 15:40:50 +0000

    DATA: File

---
+++ Standard.AI.OpenAI/Models/Services/Foundations/Files/File.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+namespace Standard.AI.OpenAI.Models.Services.Foundations.Files
+{
+    public class File
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
