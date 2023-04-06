4eaf3a8e125dad24f4529e7d46b1ad346544f89e
Gummimundur <gudm.mar@hotmail.com>
2023-04-06 15:58:52 +0000

    ShouldRemoveFileByIdAsync -> FAIL

---
+++ Standard.AI.OpenAI.Tests.Unit/Services/Foundations/Files/FileServiceTests.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Moq;
+using Standard.AI.OpenAI.Brokers.OpenAIs;
+using Standard.AI.OpenAI.Services.Foundations.Files;
+using Tynamix.ObjectFiller;
+
+namespace Standard.AI.OpenAI.Tests.Unit.Services.Foundations.Files
+{
+    public partial class FileServiceTests
+    {
+        private readonly Mock<IOpenAIBroker> openAIBrokerMock;
+        private readonly IFileService fileService;
+
+        public FileServiceTests()
+        {
+            this.openAIBrokerMock = new Mock<IOpenAIBroker>();
+
+            this.fileService = new FileService(
+                openAIBroker: this.openAIBrokerMock.Object);
+        }
+
+        private static dynamic CreateRandomFileProperties()
+        {
+            string objectType = GetRandomString();
+
+            return new
+            {
+                Id = GetRandomString(),
+                Object = objectType,
+                Type = objectType,
+                Deleted = GetRandomBoolean()
+            };
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static bool GetRandomBoolean() =>
+            Randomizer<bool>.Create();
+    }
+}
---
+++ Standard.AI.OpenAI/Services/Foundations/Files/FileService.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Standard.AI.OpenAI.Brokers.OpenAIs;
+using Standard.AI.OpenAI.Models.Services.Foundations.Files;
+
+namespace Standard.AI.OpenAI.Services.Foundations.Files
+{
+    internal class FileService : IFileService
+    {
+        private readonly IOpenAIBroker openAIBroker;
+
+        public FileService(IOpenAIBroker openAIBroker) =>
+            this.openAIBroker = openAIBroker;
+
+        public ValueTask<File> RemoveFileByIdAsync(string fileId) =>
+            throw new System.NotImplementedException();
+    }
+}
