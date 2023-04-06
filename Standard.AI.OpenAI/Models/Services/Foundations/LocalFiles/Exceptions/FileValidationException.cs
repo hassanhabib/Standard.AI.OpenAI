cfebf3d45c664c80e7e341e20e58319ce165d3ad
Hassan Habib <hassanhabib@live.com>
2023-04-05 22:37:06 -0700

    ShouldThrowValidationExceptionOnReadIfFilePathIsInvalid -> FAIL

---
+++ Standard.AI.OpenAI/Models/Services/Foundations/LocalFiles/Exceptions/FileValidationException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace Standard.AI.OpenAI.Models.Services.Foundations.LocalFiles.Exceptions
+{
+    internal class FileValidationException : Xeption
+    {
+        public FileValidationException(Xeption innerException)
+            : base(message: "File validation error occurred, fix error and try again.",
+                  innerException)
+        { }
+    }
+}
