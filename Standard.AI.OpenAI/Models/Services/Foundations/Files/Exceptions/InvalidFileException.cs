c75f201d08b27494b7a927df600e2389cd2a4122
Gummimundur <gudm.mar@hotmail.com>
2023-04-06 16:11:43 +0000

    ShouldThrowValidationExceptionOnRemoveByIdIfIdIsInvalidAsync -> FAIL

---
+++ Standard.AI.OpenAI/Models/Services/Foundations/Files/Exceptions/InvalidFileException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace Standard.AI.OpenAI.Models.Services.Foundations.Files.Exceptions
+{
+    public class InvalidFileException : Xeption
+    {
+        public InvalidFileException()
+            : base(message: "Invalid file error occurred, fix errors and try again.")
+        { }
+    }
+}
