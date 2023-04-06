521fcf5347003e5be7b249b18ed2a1d6cfee9c1e
Gummimundur <gudm.mar@hotmail.com>
2023-04-06 16:01:04 +0000

    ShouldRemoveFileByIdAsync -> PASS

--- Standard.AI.OpenAI/Services/Foundations/Files/FileService.cs
+++ Standard.AI.OpenAI/Services/Foundations/Files/FileService.cs
@@ -4,6 +4,7 @@
 
 using System.Threading.Tasks;
 using Standard.AI.OpenAI.Brokers.OpenAIs;
+using Standard.AI.OpenAI.Models.Services.Foundations.ExternalFiles;
 using Standard.AI.OpenAI.Models.Services.Foundations.Files;
 
 namespace Standard.AI.OpenAI.Services.Foundations.Files
@@ -15,7 +16,21 @@ namespace Standard.AI.OpenAI.Services.Foundations.Files
         public FileService(IOpenAIBroker openAIBroker) =>
             this.openAIBroker = openAIBroker;
 
-        public ValueTask<File> RemoveFileByIdAsync(string fileId) =>
-            throw new System.NotImplementedException();
+        public async ValueTask<File> RemoveFileByIdAsync(string fileId)
+        {
+            ExternalFile removedFile = await this.openAIBroker.DeleteFileByIdAsync(fileId);
+
+            return ConvertToFile(removedFile);
+        }
+
+        private static File ConvertToFile(ExternalFile externalFile)
+        {
+            return new File()
+            {
+                Id = externalFile.Id,
+                Type = externalFile.Object,
+                Deleted = externalFile.Deleted
+            };
+        }
     }
 }
