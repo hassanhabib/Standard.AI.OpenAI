e0bfd821dcfb60277a7b7e8abee162cc4ef75ea0
Kailu Hu <hukailu@piorsoft.com>
2023-04-07 23:54:19 -0700

    ShouldUploadFileAsync -> WIP

--- Standard.AI.OpenAI/Services/Foundations/AIFiles/AIFileService.cs
+++ Standard.AI.OpenAI/Services/Foundations/AIFiles/AIFileService.cs
@@ -2,6 +2,8 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Standard.AI.OpenAI.Brokers.DateTimes;
 using Standard.AI.OpenAI.Brokers.OpenAIs;
@@ -22,9 +24,38 @@ namespace Standard.AI.OpenAI.Services.Foundations.AIFiles
             this.dateTimeBroker = dateTimeBroker;
         }
 
-        public ValueTask<AIFile> UploadFileAsync(AIFile file)
+        public async ValueTask<AIFile> UploadFileAsync(AIFile file)
         {
-            throw new System.NotImplementedException();
+            ExternalAIFileResponse externalAIFileResponse = await PostFileAsync(file);
+            file.Response = ConvertToFileResponse(externalAIFileResponse);
+
+            return file;
+        }
+
+        private async ValueTask<ExternalAIFileResponse> PostFileAsync(AIFile file)
+        {
+            var externalAIFileRequest = new ExternalAIFileRequest()
+            {
+                File = new StreamContent(file.Request.Content),
+                Purpose = file.Request.Purpose
+            };
+
+            return await this.openAIBroker.PostFileFormAsync(externalAIFileRequest);
+        }
+
+        private AIFileResponse ConvertToFileResponse(ExternalAIFileResponse externalAIFileResponse)
+        {
+            return new AIFileResponse
+            {
+                Id = externalAIFileResponse.Id,
+                Type = externalAIFileResponse.Object,
+                Size = externalAIFileResponse.Bytes,
+                Name = externalAIFileResponse.FileName,
+                Purpose = externalAIFileResponse.Purpose,
+
+                CreatedDate =
+                    this.dateTimeBroker.ConvertToDateTimeOffSet(externalAIFileResponse.CreatedDate),
+            };
         }
     }
 }
