9ebf4aed961302be8c927b98b9fa3cf46570125e
Rami Abughazaleh <icnocop@users.noreply.github.com>
2023-04-08 17:07:37 -0700

    ShouldUploadFileAsync -> PASS

--- Standard.AI.OpenAI.Tests.Unit/Services/Foundations/AIFiles/AIFileServiceTests.cs
+++ Standard.AI.OpenAI.Tests.Unit/Services/Foundations/AIFiles/AIFileServiceTests.cs
@@ -3,11 +3,16 @@
 // ----------------------------------------------------------------------------------
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq.Expressions;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using KellermanSoftware.CompareNetObjects;
+using KellermanSoftware.CompareNetObjects.TypeComparers;
 using Moq;
 using RESTFulSense.Exceptions;
 using Standard.AI.OpenAI.Brokers.DateTimes;
@@ -30,7 +35,15 @@ namespace Standard.AI.OpenAI.Tests.Unit.Services.Foundations.AIFiles
         {
             this.openAiBrokerMock = new Mock<IOpenAIBroker>();
             this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
-            this.compareLogic = new CompareLogic();
+
+            var comparisonConfig = new ComparisonConfig();
+
+            // CompareLogic doesn't support comparing collections with a non integer indexer
+            comparisonConfig.CustomPropertyComparer<HttpContentHeaders>(
+                httpContentHeaders => httpContentHeaders.NonValidated,
+                new HttpContentHeadersTypeComparer());
+
+            this.compareLogic = new CompareLogic(comparisonConfig);
 
             this.aiFileService = new AIFileService(
                 openAIBroker: this.openAiBrokerMock.Object,
@@ -114,9 +127,146 @@ namespace Standard.AI.OpenAI.Tests.Unit.Services.Foundations.AIFiles
                     .GetValue();
 
             byte[] buffer = Encoding.UTF8.GetBytes(randomContent);
-            var memoryStream = new MemoryStream(buffer);
+            var memoryStream = new TestableStream(buffer);
 
             return memoryStream;
+        }
+    }
+
+    internal class TestableStream : MemoryStream
+    {
+        public TestableStream(byte[] buffer)
+            : base(buffer)
+        {
+        }
+
+        /// <summary>
+        /// We have to override ReadTimeout because the base implementation
+        /// throws an exception which causes FluentAssertions to fail.
+        /// </summary>
+        public override int ReadTimeout => 0;
+
+        /// <summary>
+        /// We have to override WriteTimeout because the base implementation
+        /// throws an exception which causes FluentAssertions to fail.
+        /// </summary>
+        public override int WriteTimeout => 0;
+    }
+
+    internal class HttpContentHeadersTypeComparer : DictionaryComparer
+    {
+        public HttpContentHeadersTypeComparer()
+            : base(RootComparerFactory.GetRootComparer())
+        {
+        }
+
+        public override void CompareType(CompareParms parms)
+        {
+            try
+            {
+                parms.Result.AddParent(parms.Object1);
+                parms.Result.AddParent(parms.Object2);
+
+                // objects must be the same length
+                bool countsDifferent = DictionaryCountsDifferent(parms);
+
+                if (countsDifferent && parms.Result.ExceededDifferences)
+                    return;
+
+                CompareByKeys(parms);
+            }
+            finally
+            {
+                parms.Result.RemoveParent(parms.Object1);
+                parms.Result.RemoveParent(parms.Object2);
+            }
+        }
+
+        private void CompareByKeys(CompareParms parms)
+        {
+            var dict1 = (IReadOnlyDictionary<string, HeaderStringValues>)parms.Object1;
+            var dict2 = (IReadOnlyDictionary<string, HeaderStringValues>)parms.Object2;
+
+            if (dict1 != null)
+            {
+                foreach (var key in dict1.Keys)
+                {
+                    string currentBreadCrumb = AddBreadCrumb(parms.Config, parms.BreadCrumb, "[" + key.ToString() + "].Value");
+
+                    CompareParms childParms = new CompareParms
+                    {
+                        Result = parms.Result,
+                        Config = parms.Config,
+                        ParentObject1 = parms.Object1,
+                        ParentObject2 = parms.Object2,
+                        Object1 = dict1[key],
+                        Object2 = (dict2 != null) && dict2.ContainsKey(key) ? dict2[key] : null,
+                        BreadCrumb = currentBreadCrumb
+                    };
+
+                    RootComparer.Compare(childParms);
+
+                    if (parms.Result.ExceededDifferences)
+                        return;
+                }
+            }
+
+            if (dict2 != null)
+            {
+                foreach (var key in dict2.Keys)
+                {
+                    if (dict1 != null && dict1.ContainsKey(key))
+                        continue;
+
+                    var currentBreadCrumb = AddBreadCrumb(parms.Config, parms.BreadCrumb,
+                        "[" + key.ToString() + "].Value");
+
+                    var childParms = new CompareParms
+                    {
+                        Result = parms.Result,
+                        Config = parms.Config,
+                        ParentObject1 = parms.Object1,
+                        ParentObject2 = parms.Object2,
+                        Object1 = null,
+                        Object2 = dict2[key],
+                        BreadCrumb = currentBreadCrumb
+                    };
+
+                    RootComparer.Compare(childParms);
+
+                    if (parms.Result.ExceededDifferences)
+                        return;
+
+                }
+            }
+        }
+
+        private bool DictionaryCountsDifferent(CompareParms parms)
+        {
+            IDictionary iDict1 = parms.Object1 as IDictionary;
+            IDictionary iDict2 = parms.Object2 as IDictionary;
+
+            int iDict1Count = (iDict1 == null) ? 0 : iDict1.Count;
+            int iDict2Count = (iDict2 == null) ? 0 : iDict2.Count;
+
+            if (iDict1Count == iDict2Count)
+                return false;
+
+            Difference difference = new Difference
+            {
+                ParentObject1 = parms.ParentObject1,
+                ParentObject2 = parms.ParentObject2,
+                PropertyName = parms.BreadCrumb,
+                Object1Value = iDict1Count.ToString(CultureInfo.InvariantCulture),
+                Object2Value = iDict2Count.ToString(CultureInfo.InvariantCulture),
+                ChildPropertyName = "Count",
+                Object1 = iDict1,
+                Object2 = iDict2
+            };
+
+            AddDifference(parms.Result, difference);
+
+            return true;
         }
     }
 }
