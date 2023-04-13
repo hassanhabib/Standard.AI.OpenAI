3c08d9dd1a255cf711c780a8f6c5c75d99a5fe44
Hassan Habib <hassanhabib@live.com>
2023-04-12 22:43:43 -0700

    ShouldThrowValidationExceptionOnUploadIfAIFileRquestIsInvalid -> FAIL

--- Standard.AI.OpenAI.Tests.Unit/Services/Foundations/AIFiles/AIFileServiceTests.Validations.Uplaod.cs
+++ Standard.AI.OpenAI.Tests.Unit/Services/Foundations/AIFiles/AIFileServiceTests.Validations.Uplaod.cs
@@ -59,7 +59,61 @@ namespace Standard.AI.OpenAI.Tests.Unit.Services.Foundations.AIFiles
                 new AIFileValidationException(invalidAIFileException);
 
             // when
-            ValueTask<AIFile> uploadFileTask = 
+            ValueTask<AIFile> uploadFileTask =
+                this.aiFileService.UploadFileAsync(invalidAIFile);
+
+            AIFileValidationException actualAIFileValidationException =
+                await Assert.ThrowsAsync<AIFileValidationException>(uploadFileTask.AsTask);
+
+            // then
+            actualAIFileValidationException.Should().BeEquivalentTo(
+                expectedAIFileValidationException);
+
+            this.openAiBrokerMock.Verify(broker =>
+                broker.PostFileFormAsync(It.IsAny<ExternalAIFileRequest>()),
+                    Times.Never);
+
+            this.openAiBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public async Task ShouldThrowValidationExceptionOnUploadIfAIFileRquestIsInvalid(
+            string invalidString)
+        {
+            // given
+            var invalidAIFile = new AIFile();
+
+            invalidAIFile.Request = new AIFileRequest
+            {
+                Name = invalidString,
+                Purpose = invalidString,
+                Content = null
+            };
+
+            var invalidAIFileException =
+                new InvalidAIFileException();
+
+            invalidAIFileException.AddData(
+                key: nameof(AIFileRequest.Name),
+                values: "Value is required");
+
+            invalidAIFileException.AddData(
+                key: nameof(AIFileRequest.Content),
+                values: "Value is required");
+
+            invalidAIFileException.AddData(
+                key: nameof(AIFileRequest.Purpose),
+                values: "Value is required");
+
+            var expectedAIFileValidationException =
+                new AIFileValidationException(invalidAIFileException);
+
+            // when
+            ValueTask<AIFile> uploadFileTask =
                 this.aiFileService.UploadFileAsync(invalidAIFile);
 
             AIFileValidationException actualAIFileValidationException =
