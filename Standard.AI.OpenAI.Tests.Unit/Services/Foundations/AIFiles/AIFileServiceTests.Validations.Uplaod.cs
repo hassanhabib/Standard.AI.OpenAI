2b5a74c5f9290537b7f32aa0eaa280a5e5111181
ElbekDeveloper <e.normurodov@mail.ru>
2023-04-13 10:28:18 +0500

    ShouldThrowValidationExceptionOnUploadIfAIFileIsNull -> FAIL

---
+++ Standard.AI.OpenAI.Tests.Unit/Services/Foundations/AIFiles/AIFileServiceTests.Validations.Uplaod.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standard.AI.OpenAI.Models.Services.Foundations.AIFiles;
+using Standard.AI.OpenAI.Models.Services.Foundations.AIFiles.Exceptions;
+using Xunit;
+
+namespace Standard.AI.OpenAI.Tests.Unit.Services.Foundations.AIFiles
+{
+    public partial class AIFileServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnUploadIfAIFileIsNull()
+        {
+            // given
+            AIFile noAIFile = null;
+            var nullAIFileException = new NullAIFileException();
+
+            var expectedAIFileValidationException =
+                new AIFileValidationException(nullAIFileException);
+
+            // when
+            ValueTask<AIFile> uploadFileTask = this.aiFileService.UploadFileAsync(noAIFile);
+
+            AIFileValidationException actualAIFileValidationException =
+                await Assert.ThrowsAsync<AIFileValidationException>(uploadFileTask.AsTask);
+
+            // then
+            actualAIFileValidationException.Should().BeEquivalentTo(
+                expectedAIFileValidationException);
+
+            this.openAiBrokerMock.Verify(broker =>
+                broker.PostFileFormAsync(It.IsAny<ExternalAIFileRequest>()),
+                    Times.Never);
+
+            this.openAiBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Standard.AI.OpenAI/Models/Services/Foundations/AIFiles/Exceptions/NullAiFileException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace Standard.AI.OpenAI.Models.Services.Foundations.AIFiles.Exceptions
+{
+    public partial class NullAIFileException : Xeption
+    {
+        public NullAIFileException()
+            : base(message: "Ai file is null.")
+        { }
+    }
+}
