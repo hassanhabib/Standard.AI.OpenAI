41b6aa78750e90df0ebea2ece0590f82fb5f05e9
Hassan Habib <hassanhabib@live.com>
2023-04-17 22:23:35 -0700

    ShouldThrowValidationExceptionOnUploadIfFileIsNullAsync -> FAIL

---
+++ Standard.AI.OpenAI.Tests.Unit/Services/Orchestrations/AIFiles/AIFileOrchestrationServiceTests.Validations.Upload.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standard.AI.OpenAI.Models.Services.Foundations.AIFiles;
+using Standard.AI.OpenAI.Models.Services.Orchestrations.AIFiles.Exceptions;
+using Xunit;
+
+namespace Standard.AI.OpenAI.Tests.Unit.Services.Orchestrations.AIFiles
+{
+    public partial class AIFileOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnUploadIfFileIsNullAsync()
+        {
+            // given
+            AIFile nullAIFile = null;
+
+            var nullAIFileOrchestrationException =
+                new NullAIFileOrchestrationException();
+
+            var expectedAIFileOrchestrationValidationException =
+                new AIFileOrchestrationValidationException(
+                    nullAIFileOrchestrationException);
+
+            // when
+            ValueTask<AIFile> uploadFileTask =
+                this.aiFileOrchestrationService.UploadFileAsync(
+                    nullAIFile);
+
+            AIFileOrchestrationValidationException
+                actualAIFileOrchestrationValidatioNException =
+                    await Assert.ThrowsAsync<AIFileOrchestrationValidationException>(
+                        uploadFileTask.AsTask);
+
+            // then
+            actualAIFileOrchestrationValidatioNException.Should().BeEquivalentTo(
+                expectedAIFileOrchestrationValidationException);
+
+            this.localFileServiceMock.Verify(service =>
+                service.ReadFile(It.IsAny<string>()),
+                    Times.Never);
+
+            this.aiFileServiceMock.Verify(service =>
+                service.UploadFileAsync(It.IsAny<AIFile>()),
+                    Times.Never);
+
+            this.localFileServiceMock.VerifyNoOtherCalls();
+            this.aiFileServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
