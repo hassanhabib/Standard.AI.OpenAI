a8452bb19490818f831655751949b8d884aed03b
mohinur18 <muhammadjonovamohinur36@gmail.com>
2023-04-27 00:39:37 +0500

    BROKERS: Add List of Files

---
+++ Standard.AI.OpenAI/Models/Services/Foundations/ExternalAIFiles/ExternalListofAIFiles.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Standard.AI.OpenAI.Models.Services.Foundations.Files;
+
+namespace Standard.AI.OpenAI.Models.Services.Foundations.ExternalFiles
+{
+    internal class ExternalListFilesResponse
+    {
+        [JsonProperty(propertyName: "listFiles")]
+        public List<File> ListFiles { get; set; }
+    }
+}
