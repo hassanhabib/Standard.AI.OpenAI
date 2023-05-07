60420d5f97997c8ef020294c0e4ba112e9c3bdc6
Catalin-Andronie <catalin.andronie@gmail.com>
2023-05-07 11:14:27 +0300

    ShouldSendAudioTranscriptionAsync -> FAIL

---
+++ Standard.AI.OpenAI/Models/Services/Foundations/AudioTranscriptions/AudioTranscriptionResponse.cs
@@ -0,0 +1,11 @@
+// --------------------------------------------------------------- 
+// Copyright (c) Coalition of the Good-Hearted Engineers 
+// ---------------------------------------------------------------
+
+namespace Standard.AI.OpenAI.Models.Services.Foundations.AudioTranscriptions
+{
+    public class AudioTranscriptionResponse
+    {
+        public string Text { get; set; } = string.Empty;
+    }
+}
