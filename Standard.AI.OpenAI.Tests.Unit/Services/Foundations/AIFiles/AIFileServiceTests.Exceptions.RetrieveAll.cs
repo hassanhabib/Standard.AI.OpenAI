d37f335f32bb00215b5cab6735c1dadf6ad206ac
Diogo Duarte <diogo.duarte3@gmail.com>
2023-05-10 06:34:05 +0100

    ShouldThrowDependencyExceptionOnRetrieveAllIfUrlNotFoundAsync -> FAIL

---
+++ Standard.AI.OpenAI.Tests.Unit/Services/Foundations/AIFiles/AIFileServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,58 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using RESTFulSense.Exceptions;
+using Standard.AI.OpenAI.Models.Services.Foundations.AIFiles;
+using Standard.AI.OpenAI.Models.Services.Foundations.AIFiles.Exceptions;
+using Xunit;
+
+namespace Standard.AI.OpenAI.Tests.Unit.Services.Foundations.AIFiles
+{
+    public partial class AIFileServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveAllIfUrlNotFoundAsync()
+        {
+            // given
+            var httpResponseUrlNotFoundException =
+                new HttpResponseUrlNotFoundException();
+
+            var invalidConfigurationAIFileException =
+                new InvalidConfigurationAIFileException(
+                    httpResponseUrlNotFoundException);
+
+            var expectedAIFileDependencyException =
+                new AIFileDependencyException(
+                    invalidConfigurationAIFileException);
+
+            this.openAIBrokerMock.Setup(broker =>
+                broker.GetAllFilesAsync())
+                    .ThrowsAsync(httpResponseUrlNotFoundException);
+
+            // when
+            ValueTask<IEnumerable<AIFileResponse>> getAllFilesTask =
+               this.aiFileService.RetrieveAllFilesAsync();
+
+            AIFileDependencyException
+                actualAIFileDependencyException =
+                    await Assert.ThrowsAsync<AIFileDependencyException>(
+                        getAllFilesTask.AsTask);
+
+            // then
+            actualAIFileDependencyException.Should().BeEquivalentTo(
+                expectedAIFileDependencyException);
+
+            this.openAIBrokerMock.Verify(broker =>
+                broker.GetAllFilesAsync(),
+                    Times.Once);
+
+            this.openAIBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
