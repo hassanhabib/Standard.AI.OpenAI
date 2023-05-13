35a7f4173c9aa50fac2fa21ce817572f526d9c1a
Brian Parker <brian.parker@hotmail.com.au>
2023-05-13 14:02:58 +1000

    ShouldThrowValidationExceptionOnSubmitIfFineTuneIsNullAsync -> FAIL

---
+++ Standard.AI.OpenAI.Tests.Unit/Services/Foundations/FineTunes/FineTuneServiceTests.Exceptions.Submit.cs
@@ -0,0 +1,52 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standard.AI.OpenAI.Models.Services.Foundations.ExternalFineTunes;
+using Standard.AI.OpenAI.Models.Services.Foundations.FineTunes;
+using Standard.AI.OpenAI.Models.Services.Foundations.FineTunes.Exceptions;
+using Xunit;
+
+namespace Standard.AI.OpenAI.Tests.Unit.Services.Foundations.FineTunes
+{
+    public partial class FineTuneServiceTests
+    {
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnSendIfFineTuneIsNullAsync()
+        {
+            // given
+            FineTune nullFineTune = null;
+
+            var nullFineTuneException =
+                new NullFineTuneException();
+
+            var expectedFineTuneValidationException =
+                new FineTuneValidationException(nullFineTuneException);
+
+            // when
+            ValueTask<FineTune> sendFineTuneTask =
+                this.fineTuneService.SubmitFineTuneAsync(
+                    nullFineTune);
+
+            FineTuneValidationException actualFineTuneValidationException =
+                await Assert.ThrowsAsync<FineTuneValidationException>(
+                    sendFineTuneTask.AsTask);
+
+            // then
+            actualFineTuneValidationException.Should()
+                .BeEquivalentTo(expectedFineTuneValidationException);
+
+            this.openAIBrokerMock.Verify(broker =>
+                broker.PostFineTuneAsync(
+                    It.IsAny<ExternalFineTuneRequest>()),
+                        Times.Never);
+
+            this.openAIBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    } 
+}
