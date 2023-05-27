cde9434b538bea17a34d21b5e8a817cfdfe2d12c
Hassan Habib <hassanhabib@live.com>
2023-05-26 23:22:27 -0700

    INTEGRATIONS: Submit Fine Tune Request
    Closes #315

---
+++ Standard.AI.OpenAI.Tests.Integration/APIs/FineTunes/FineTuneTests.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Standard.AI.OpenAI.Clients.OpenAIs;
+using Standard.AI.OpenAI.Models.Configurations;
+using Standard.AI.OpenAI.Models.Services.Foundations.AIFiles;
+
+namespace Standard.AI.OpenAI.Tests.Integration.APIs.FineTunes
+{
+    public partial class FineTuneTests
+    {
+        private readonly IOpenAIClient openAIClient;
+
+        public FineTuneTests()
+        {
+            var openAIConfigurations = new OpenAIConfigurations
+            {
+                ApiKey = Environment.GetEnvironmentVariable("ApiKey"),
+                OrganizationId = Environment.GetEnvironmentVariable("OrgId"),
+                ApiUrl = "https://api.openai.com/"
+            };
+
+            this.openAIClient = new OpenAIClient(openAIConfigurations);
+        }
+
+        private async ValueTask<AIFile> SubmitRandomFileAsync()
+        {
+            MemoryStream memoryStream = CreateRandomStream();
+
+            var aiFile = new AIFile
+            {
+                Request = new AIFileRequest
+                {
+                    Name = "Test",
+                    Content = memoryStream,
+                    Purpose = "fine-tune"
+                }
+            };
+
+            return await this.openAIClient.AIFiles
+                .UploadFileAsync(aiFile);
+        }
+
+        private static MemoryStream CreateRandomStream()
+        {
+            string content = "{\"prompt\": \"<prompt text>\", \"completion\": \"<ideal generated text>\"}";
+
+            return new MemoryStream(Encoding.UTF8.GetBytes(content));
+        }
+    }
+}
